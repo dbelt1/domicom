caa99ebb43f30002ef2d6f23ffdc6d5121446e98
camilocetina <64600635+camilocetina@users.noreply.github.com>
2020-08-08 13:38:55 -0500

    chupelo diego y elvis

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel escrito por camilo cetina 
+                    Laravel escrito por camilo cetina 22222
                 </div>
 
                 <div class="links">
