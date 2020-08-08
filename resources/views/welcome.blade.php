a2660a5aa8f69f7b27761a4802bb1c968134fd09
camilocetina <64600635+camilocetina@users.noreply.github.com>
2020-08-08 13:05:58 -0500

    prueba del maister

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel desde camilo cetina
                 </div>
 
                 <div class="links">
