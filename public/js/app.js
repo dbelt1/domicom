5d145162c5f776b9ffafd934935ba9d693dc9d19
camilocetina <64600635+camilocetina@users.noreply.github.com>
2020-08-08 12:58:02 -0500

    comit desde master cetina

--- public/js/app.js
+++ public/js/app.js
@@ -49878,8 +49878,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\DIEGO\Desktop\nuevoProyecto\domicom\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\DIEGO\Desktop\nuevoProyecto\domicom\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\CETINA\Desktop\domicom\domicom\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\CETINA\Desktop\domicom\domicom\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
